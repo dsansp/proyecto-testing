e6921415fb6febce8b366fdb5d5a475ae3a6ee5b
ismaelai <ismamontiel@hotmail.com>
2021-11-17 21:41:01 +0100

    TelegramTest

--- src/test/java/com/example/proyectotesting/patterns/behavioral/mediator/TelegramTest.java
+++ src/test/java/com/example/proyectotesting/patterns/behavioral/mediator/TelegramTest.java
@@ -2,37 +2,50 @@ package com.example.proyectotesting.patterns.behavioral.mediator;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class TelegramTest {
 
-    Telegram telegram;
-    AbstractUser randomUser;
-
-    @BeforeEach
-    void setUp() {
-        telegram = new Telegram();
-        randomUser = new User(new Telegram(), "name");
-    }
 
     @Test
     public void sendMessageTest(){
-        randomUser = new User(new Telegram(), "name2");
-        User mockUser = mock(User.class);
-        telegram.addUser(mockUser);
+        //randomUser = new User(new Telegram(), "name2");
+        Telegram telegram = new Telegram();
+        AbstractUser user1 = mock(AbstractUser.class);
+        String message = "message";
 
-        doNothing().when(mockUser).receive("message to be delivered");
-        telegram.sendMessage("message to be delivered", randomUser);
+        doNothing().when(user1).receive(anyString());
+        telegram.addUser(user1);
 
-        verify(mockUser).receive("message to be delivered");
+        telegram.sendMessage(message, user1);
+
+        verify(user1).receive(message);
     }
 
-    //@Test
-    // public void addUserTest(){
-    // int numContacts = telegram.users.size();
-    //telegram.addUser(user);
-    //assertTrue(numContacts < telegram.users.size());
-//}
+    @Test
+    @DisplayName("añadir usuario")
+   public void addUser() {
+        Telegram telegram = new Telegram();
+        AbstractUser user = mock(AbstractUser.class);
+
+        int list = telegram.users.size();
+        telegram.addUser(user);
+
+        assertTrue(list < telegram.users.size());
+
+    }
+    @Test
+    @DisplayName("Eliminar usuario")
+   public void removeUser() {
+        Telegram telegram=new Telegram();
+
+        AbstractUser user1 = new User(telegram, "ismael");
+        int list = telegram.users.size();
+        telegram.addUser(user1);
+        telegram.removeUser(user1);
+        assertEquals(list, telegram.users.size());
+    }
 }
