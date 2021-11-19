03f82809ea92d38d778aef279395394ac855b308
ismaelai <ismamontiel@hotmail.com>
2021-11-19 14:27:43 +0100

    ManufacturerRestControllerTest fix

---
+++ src/test/java/com/example/proyectotesting/selenium/ManufacturerEditTest.java
@@ -0,0 +1,18 @@
+package com.example.proyectotesting.selenium;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class ManufacturerEditTest extends BaseTest {
+
+    private static final String URL = "http://localhost:8082/manufacturers";
+    @Disabled
+    @Test
+    void cargarFabricantePaginaTest(){
+        //webdriver.get(URL);
+        //webdriver.findElement(By.xpath("//*[@id=\"products-list\"]/tbody/tr[2]/td[5]/a")).click();
+
+    }
+
+}
