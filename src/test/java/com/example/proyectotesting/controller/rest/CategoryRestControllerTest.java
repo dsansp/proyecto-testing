3cabb424327432efb3182908cdd652630dd8c778
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-24 15:21:08 +0100

    commit selenium.yml and CategoryRestControllerTest

--- src/test/java/com/example/proyectotesting/controller/rest/CategoryRestControllerTest.java
+++ src/test/java/com/example/proyectotesting/controller/rest/CategoryRestControllerTest.java
@@ -352,6 +352,7 @@ class CategoryRestControllerTest {
 
         }
 
+        @Disabled
         @DisplayName("Comprobamos que no se borra las  categorias")
         @Test
         void deleteAllTest(){
