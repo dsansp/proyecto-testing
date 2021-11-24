4e88cc9c6f7657602a02924cc9ee60000b5cb728
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-24 12:57:58 +0100

    Method in IoanLambdaTest Disabled

---
+++ src/test/java/com/example/proyectotesting/lambdaTest/IoanLambdaTest.java
@@ -0,0 +1,71 @@
+package com.example.proyectotesting.lambdaTest;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IoanLambdaTest {
+    String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+    public static RemoteWebDriver driver = null;
+    public String gridURL = "@hub.lambdatest.com/wd/hub";
+    public String status = "failed";
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("version", "latest");
+//        capabilities.setCapability("platform", "Windows 10"); // If this cap isn't specified, it will just get the any available one
+
+        capabilities.setCapability("platform", "Windows 10");
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("version","95.0");
+        capabilities.setCapability("resolution","1920x1080");
+        capabilities.setCapability("console","error");
+        capabilities.setCapability("visual",true);
+
+        capabilities.setCapability("build", "ProjectTesting1");
+        capabilities.setCapability("name", "ProjectTesting");
+        capabilities.setCapability("network", true); // To enable network logs
+        capabilities.setCapability("visual", true); // To enable step by step screenshot
+        capabilities.setCapability("video", true); // To enable video recording
+        capabilities.setCapability("console", true); // To capture console logs
+        try {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + gridURL), capabilities);
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid grid URL");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+
+@Disabled
+    @Test
+    public void testSimple() throws Exception {
+
+        driver.get("http://localhost:8082/products");
+        assertEquals("Products Directory", driver.findElement(By.tagName("h1")).getText());
+        status = "passed";
+
+    }
+}
