8c08944947fe2f2fcc947cc27c7566cd8c1f9893
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-24 17:56:47 +0100

    ProductListTest added v2

--- src/test/java/selenium/ProductListTest.java
+++ src/test/java/selenium/ProductListTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductListTest extends BaseTest {
 
 
     //private static final String URL = "https://proyectogrupo1testing.herokuapp.com/products";
+    private static final String URL0 = "http://localhost:8082/products/new";
     private static final String URL1 = "http://localhost:8082/products";
     private static final String URL2 = "http://localhost:8082/products/9/view";
 
@@ -25,38 +28,88 @@ public class ProductListTest extends BaseTest {
 
     }
 
+   @DisplayName("Creando un producto para luego poder comprobar que se ve")
     @Test
-    void buttonVerTest(){
-        driver.get(URL2);
-        assertEquals("Nombre", driver.findElement(By.xpath("//div/p[1]/b")).getText());
-       // assertEquals(": Balón", driver.findElement(By.xpath("/html/body/div/div/div/p[1]/text()")).getText());
+    void buttonVerProductTest(){
+        driver.get(URL1);
+        driver.getCurrentUrl();
 
-        assertEquals("Descripción", driver.findElement(By.xpath("//div/div/div/p[2]/b")).getText());
+        driver.findElement(By.xpath("//div/p[2]/a[1]")).click();
+        driver.get(URL0);
 
-        assertEquals("Precio",driver.findElement(By.xpath("//*/div/div/p[3]/b")).getText());
+        driver.getCurrentUrl();
 
-        assertEquals("Cantidad", driver.findElement(By.xpath("//*/div/div/p[4]/b")).getText());
+        //Creando el producto de PS3
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='name']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='name']")).sendKeys("PS3");
+        driver.findElement(By.xpath("//div[@class='form-group']//textarea")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//textarea")).sendKeys("PS3 la mejor consola del año");
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='price']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='price']")).sendKeys("260");
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='quantity']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='quantity']")).sendKeys("1");
 
-//        driver.get("http://localhost:8082/manufacturers/1/view");
-//        assertEquals("Fabricante 1", driver.findElement(By.tagName("h2")).getText());
-//
-//        WebElement buttonVolver = driver.findElement(By.xpath("//div/div/a[1]"));
-//        buttonVolver.click();
 
-        assertEquals("Fabricante", driver.findElement(By.xpath("//*/div/div/p[5]/b")).getText());
-        assertEquals("Categorías asociadas", driver.findElement(By.tagName("h3")).getText());
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"product\"]/div[7]/button"));
+        button.submit();
 
-        assertEquals("Libros",driver.findElement(By.xpath("//div/ul/li[1]")).getText());
-        assertEquals("Computación", driver.findElement(By.xpath("//div/ul/li[2]/span")).getText());
+        List <WebElement> productos = driver.findElements(By.tagName("tr"));
+        int numero = productos.size();
 
-    }
+        //Comprobaciones
+        String title = driver.getTitle();
+        assertEquals("Product List | Awesome App",title);
 
-    @Test
-    void VolverAProductoListTest() {
-        driver.get(URL2);
-        driver.findElement(By.className("btn-info")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"products-list\"]/tbody/tr["+ numero +"]/td[7]/a[1]")).click();
+        String titleProduct = driver.getTitle();
+
+        assertEquals("Nombre: PS3", driver.findElement(By.xpath("//div/p[1]")).getText());
+        assertEquals("Descripción: PS3 la mejor consola del año", driver.findElement(By.xpath("//div/p[2]")).getText());
+
+        driver.findElement(By.xpath("//div/a[1]")).click();
+
         assertEquals("Products Directory", driver.findElement(By.tagName("h1")).getText());
+
     }
+    @DisplayName("Borrando el producto que hemos creado anteriormente")
+    @Test
+    void buttonDeleteProductTest(){
+        driver.get(URL1);
+        driver.getCurrentUrl();
+
+        driver.findElement(By.xpath("//div/p[2]/a[1]")).click();
+        driver.get(URL0);
+
+        driver.getCurrentUrl();
+
+        //Creando el producto de Wii para borrarlo
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='name']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='name']")).sendKeys("Wii");
+        driver.findElement(By.xpath("//div[@class='form-group']//textarea")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//textarea")).sendKeys("Wii 2000 la mejor consola del año");
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='price']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='price']")).sendKeys("300");
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='quantity']")).clear();
+        driver.findElement(By.xpath("//div[@class='form-group']//input[@id='quantity']")).sendKeys("1");
+
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"product\"]/div[7]/button"));
+        button.submit();
+
+        List <WebElement> productos = driver.findElements(By.tagName("tr"));
+        int numero = productos.size();
+
+        driver.findElement(By.xpath("//*[@id=\"products-list\"]/tbody/tr["+ numero +"]/td[7]/a[1]")).click();
+        String titleProduct = driver.getTitle();
+
+        assertEquals("Nombre: Wii", driver.findElement(By.xpath("//div/p[1]")).getText());
+        assertEquals("Descripción: Wii 2000 la mejor consola del año", driver.findElement(By.xpath("//div/p[2]")).getText());
+
+        //Volver a la lista de productos
+        driver.findElement(By.xpath("//div/a[3]")).click();
+    }
+
+
 
 
 }
